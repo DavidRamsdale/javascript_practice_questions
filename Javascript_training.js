be1a1677442576884d11538dcecc74b4f06a7be8
David Ramsdale <davidramsdale1991@gmail.com>
2018-11-13 17:13:39 +1100

    Committing answers to problems so far

---
+++ Javascript_training.js
@@ -0,0 +1,33 @@
+// Ruby vs Javascript
+// Challenge
+
+(user.age) = prompt("Please enter your age", "age");
+if (user.age >= 18) {
+console.log("Welcome to casino")}
+else{
+console.log("Too young mate")}
+
+// Beast mode +
+list = []
+
+for (i = 1; i < 4; i++){
+	let name4 = prompt("Please enter in your name")	
+	list.push(name4)};
+alert(list);
+
+// Beast mode ++
+
+students = [{name: "David", age:27}]
+
+
+// Object 
+// Challenge
+
+for (i = 0; i< me.hobbies.length; i ++) {
+	console.log(me.hobbies[i])
+};
+
+// Beast mode
+// Adding to the array
+me.hobbies.push("food")
+
